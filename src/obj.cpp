fc7a7eac0c52bd8d8b0d5f26a1133dc9de840dfb
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2020-06-16 15:47:51 +0100

    Added files for obj.

---
+++ src/obj.cpp
@@ -0,0 +1,35 @@
+#include "evulkan_core.h"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+
+void evk::loadOBJ(const std::string &fileName, std::vector<Vertex> &vertices, std::vector<uint32_t> &indices)
+{
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    std::string warn, err;
+
+    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &warn, &err, fileName.c_str())) {
+        throw std::runtime_error(warn + err);
+    }
+
+    for (const auto &shape : shapes)
+    {
+        for (const auto &index : shape.mesh.indices)
+        {
+            Vertex vertex{};
+            vertex.pos = {
+                attrib.vertices[3 * index.vertex_index + 0],
+                attrib.vertices[3 * index.vertex_index + 1],
+                attrib.vertices[3 * index.vertex_index + 2]
+            };
+            vertex.color = {0.1,0.1,0.1};
+            
+            vertices.push_back(vertex);
+            indices.push_back(indices.size());
+        }
+    }
+
+    printf("loaded OBJ %s\n", fileName.c_str());
+}
