be34554f5d44708b90f91202e14ec1da26c1614c
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2020-07-28 10:40:07 +0100

    Added raii to descriptor.

--- include/descriptor.h
+++ include/descriptor.h
@@ -15,8 +15,8 @@ class Descriptor
     Descriptor()=default;
     Descriptor(const Descriptor&)=delete;
     Descriptor& operator=(const Descriptor&)=delete;
-    Descriptor(Descriptor&&)=delete;
-    Descriptor& operator=(Descriptor&&)=delete;
+    Descriptor(Descriptor&&) noexcept;
+    Descriptor& operator=(Descriptor&&) noexcept;
     ~Descriptor() noexcept;
 
     Descriptor(
@@ -25,57 +25,74 @@ class Descriptor
         const size_t numAttachments
     );
 
+    bool operator==(const Descriptor&) const;
+
     void addUniformBuffer(
         const uint32_t binding,
         const Buffer &buffer,
         const Shader::Stage shaderStage,
         const VkDeviceSize bufferSize
     );
-
     void addInputAttachment(
         const uint32_t binding,
         const Attachment &attachment,
         const Shader::Stage shaderStage
     );
-
     void addTextureSampler(
         const uint32_t binding,
         const Texture &texture,
         const Shader::Stage shaderStage
     );
-
     void allocateDescriptorPool();
     void allocateDescriptorSets();
 
-    std::vector<VkDescriptorSetLayout> setLayouts() const { return m_descriptorSetLayouts; };
-    std::vector<VkDescriptorSet> sets() const { return m_descriptorSets; };
+    std::vector<VkDescriptorSetLayout> setLayouts() const { return m_setLayouts; };
+    std::vector<VkDescriptorSet> sets() const { return m_sets; };
 
     private:
+    VkDescriptorBufferInfo m_bufferInfo;
     VkDevice m_device;
-    VkDescriptorPool m_descriptorPool;
-    std::vector<VkDescriptorSetLayout> m_descriptorSetLayouts;
-    std::vector<VkDescriptorSet> m_descriptorSets;
-    std::vector<VkWriteDescriptorSet> m_writeDescriptorSetVertex;
-    std::vector<VkWriteDescriptorSet> m_writeDescriptorSetFragment;
-    std::vector<VkDescriptorPoolSize> m_descriptorPoolSizes;
-    std::vector<VkDescriptorSetLayoutBinding> m_descriptorSetBindings;
-    VkDescriptorBufferInfo m_descriptorBufferInfo;
-    VkDescriptorImageInfo m_descriptorTextureSamplerInfo;
-    std::vector<VkDescriptorImageInfo> m_descriptorInputAttachmentInfo;
-    size_t m_swapchainSize;
+    std::vector<VkDescriptorImageInfo> m_inputAttachmentInfo;
     size_t m_numAttachments;
-    size_t numUniformBuffers=0;
-    size_t numInputAttachments=0;
-    size_t numImageSamplers=0;
+    size_t m_numInputAttachments=0;
+    size_t m_numImageSamplers=0;
+    size_t m_numUniformBuffers=0;
+    VkDescriptorPool m_pool;
+    std::vector<VkDescriptorPoolSize> m_poolSizes;
+    std::vector<VkDescriptorSetLayout> m_setLayouts;
+    std::vector<VkDescriptorSetLayoutBinding> m_setBindings;
+    std::vector<VkDescriptorSet> m_sets;
+    size_t m_swapchainSize;
+    VkDescriptorImageInfo m_textureSamplerInfo;
+    std::vector<VkWriteDescriptorSet> m_writeSetFragment;
+    std::vector<VkWriteDescriptorSet> m_writeSetVertex;
 
-    void addDescriptorPoolSize(const VkDescriptorType type, const size_t count);
-    void addDescriptorSetBinding(const VkDescriptorType type, uint32_t binding, VkShaderStageFlagBits stage);
-    
-    void addWriteDescriptorSetTextureSampler(const Texture &texture, uint32_t binding, VkShaderStageFlagBits stage);
+    void addDescriptorPoolSize(
+        const VkDescriptorType type,
+        const size_t count
+    );
+    void addDescriptorSetBinding(
+        const VkDescriptorType type,
+        uint32_t binding,
+        VkShaderStageFlagBits stage
+    );
+    void addWriteDescriptorSetTextureSampler(
+        const Texture &texture,
+        uint32_t binding,
+        VkShaderStageFlagBits stage
+    );
     void addWriteDescriptorSetBuffer(
-        VkBuffer buffer, VkDeviceSize range,
-        uint32_t binding, VkDescriptorType type, VkShaderStageFlagBits stage);
-    void addWriteDescriptorSetInputAttachment(VkImageView imageView, uint32_t binding, VkShaderStageFlagBits stage);
+        VkBuffer buffer,
+        VkDeviceSize range,
+        uint32_t binding,
+        VkDescriptorType type,
+        VkShaderStageFlagBits stage
+    );
+    void addWriteDescriptorSetInputAttachment(
+        VkImageView imageView,
+        uint32_t binding,
+        VkShaderStageFlagBits stage
+    );
 };
 
 #endif
