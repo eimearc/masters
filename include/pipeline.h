9e69e0832e8a914cda06afe18dd38f477d6dded6
Eimear Crotty <eimear.crotty@umail.ucc.ie>
2020-08-03 19:45:24 +0100

    Added documentation to Pipeline.

--- include/pipeline.h
+++ include/pipeline.h
@@ -7,10 +7,33 @@
 #include "shader.h"
 #include "pass.h"
 #include "util.h"
+#include "vertexinput.h"
 #include <vulkan/vulkan.h>
 
-#include "vertexinput.h"
-
+/**
+ * @class Pipeline
+ * @brief A Pipeline is a sequence of operations which produces an image.
+ * 
+ * A Pipeline is constructed using all the state required to pass data from the
+ * input assembly stage all the way to setting pixels. Each Pipeline has a
+ * Subpass, Descriptor, VertexInput, Renderpass and set of Shaders.
+ * 
+ * A Pipeline sets up the fixed function stages of the program, along with
+ * the custom information provided for setting up the programmable stages,
+ * such as the vertex and fragment Shaders.
+ * 
+ * The Pipeline is then passed into the finalize method of the Device.
+ * 
+ * @example
+ * Pipeline pipeline0(
+ *  device, &subpass0, &descriptor0, vertexInput0, &renderpass, shaders0
+ * );
+ * Pipeline pipeline1(
+ *  device, &subpass1, &descriptor1, vertexInput1, &renderpass, shaders1
+ * );
+ * std::vector<Pipeline*> pipelines = {&pipeline0, &pipeline1};
+ * device.finalize(indexBuffer,vertexBuffer,pipelines);
+ **/
 class Pipeline
 {
     public:
@@ -21,14 +44,32 @@ class Pipeline
     Pipeline& operator=(Pipeline&&) noexcept;
     ~Pipeline() noexcept;
 
+    /**
+     * Creates a Pipeline.
+     * @param[in] device the Device used to create the Pipeline.
+     * @param[in] pSubpass the Subpass to use in this Pipeline.
+     * @param[in] pDescriptor the Descriptor to use in this Pipeline.
+     * @param[in] vertexInput the vertexInput for this Pipeline.
+     * @param[in] pRenderpass the Renderpass for this Pipeline.
+     * @param[in] shaders the set of Shaders used in this Pipeline.
+     **/
     Pipeline(
         Device &device,
-        Subpass *pSubpass, // TODO: For everything: reference or pointer?
+        Subpass *pSubpass,
         gsl::not_null<Descriptor*> pDescriptor,
         const VertexInput &vertexInput,
         Renderpass *pRenderpass,
         const std::vector<Shader*> &shaders
     );
+
+    /**
+     * Creates a Pipeline.
+     * @param[in] device the Device used to create the Pipeline.
+     * @param[in] pSubpass the Subpass to use in this Pipeline.
+     * @param[in] vertexInput the vertexInput for this Pipeline.
+     * @param[in] pRenderpass the Renderpass for this Pipeline.
+     * @param[in] shaders the set of Shaders used in this Pipeline.
+     **/
     Pipeline(
         Device &device,
         Subpass *pSubpass,
